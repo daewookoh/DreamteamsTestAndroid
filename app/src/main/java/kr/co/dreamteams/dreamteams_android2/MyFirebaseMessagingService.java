1ec69fa74bd909531d00765d469fa6bac3320f53
godowondev <bdkfasd@naver.com>
2018-07-18 14:35:11 +0900

    push 메세지창 보이기

--- app/src/main/java/kr/co/dreamteams/dreamteams_android2/MyFirebaseMessagingService.java
+++ app/src/main/java/kr/co/dreamteams/dreamteams_android2/MyFirebaseMessagingService.java
@@ -1,22 +1,30 @@
 package kr.co.dreamteams.dreamteams_android2;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
+import android.os.PowerManager;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.Map;
+
 public class MyFirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
 
     private static final String TAG = "TTT";
+    Common common = new Common(this);
 
     /**
      * Called when message is received.
@@ -30,71 +38,64 @@ public class MyFirebaseMessagingService extends com.google.firebase.messaging.Fi
         // TODO(developer): Handle FCM messages here.
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.getFrom());
+        long start_time = System.currentTimeMillis();
+        common.log(Long.toString(start_time));
 
-        // Check if message contains a data payload.
-        if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        Map<String, String> data = remoteMessage.getData();
 
-            if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-            } else {
-                // Handle message within 10 seconds
-                handleNow();
-            }
+        //you can get your text message here.
+        String msg= data.get("msg");
+        String title= data.get("title");
 
-        }
+        NotificationCompat.Builder mBuilder = createNotification(title, msg);
+        mBuilder.setContentIntent(createPendingIntent());
 
-        // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-            sendNotification(remoteMessage.getNotification().getBody());
-        }
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(1, mBuilder.build());
 
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
     }
     // [END receive_message]
 
     /**
-     * Handle time allotted to BroadcastReceivers.
+     * 노티피케이션을 누르면 실행되는 기능을 가져오는 노티피케이션
+     *
+     * 실제 기능을 추가하는 것
+     * @return
      */
-    private void handleNow() {
-        Log.d(TAG, "Short lived task is done.");
+    private PendingIntent createPendingIntent(){
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+
+        return stackBuilder.getPendingIntent(
+                0,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
     }
 
     /**
-     * Create and show a simple notification containing the received FCM message.
-     *
-     * @param messageBody FCM message body received.
+     * 노티피케이션 빌드
+     * @return
      */
-    private void sendNotification(String messageBody) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-                PendingIntent.FLAG_ONE_SHOT);
-
+    private NotificationCompat.Builder createNotification(String title, String msg){
         String channelId = "CHANNEL_ID";
-        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder =
-                new NotificationCompat.Builder(this, channelId)
-                        .setSmallIcon(R.mipmap.ic_launcher)
-                        .setContentTitle("FCM Message")
-                        .setContentText(messageBody)
-                        .setAutoCancel(true)
-                        .setSound(defaultSoundUri)
-                        .setContentIntent(pendingIntent);
-
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        // Since android Oreo notification channel is needed.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            // Create channel to show notifications.
-            String channelName = "CHANNEL_NAME";
-            NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
-            notificationManager.createNotificationChannel(channel);
+        Bitmap icon = BitmapFactory.decodeResource(getResources(), R.mipmap.icon);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.mipmap.icon)
+                .setLargeIcon(icon)
+                .setContentTitle(title)
+                .setContentText(msg)
+                .setSmallIcon(R.mipmap.icon/*스와이프 전 아이콘*/)
+                .setAutoCancel(true)
+                .setWhen(System.currentTimeMillis())
+                .setDefaults(Notification.DEFAULT_ALL);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            builder.setCategory(Notification.CATEGORY_MESSAGE)
+                    .setPriority(Notification.PRIORITY_HIGH)
+                    .setVisibility(Notification.VISIBILITY_PUBLIC);
         }
-
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        return builder;
     }
+
 }
