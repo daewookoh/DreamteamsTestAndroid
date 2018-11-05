85290a4b27098b38b340bffa1511a7897b9c3d34
godowondev <bdkfasd@naver.com>
2018-11-05 10:23:41 +0900

    만보기 소스 업데이트

--- app/src/main/java/kr/co/dreamteams/dreamteams_android2/StepCheckService.java
+++ app/src/main/java/kr/co/dreamteams/dreamteams_android2/StepCheckService.java
@@ -2,6 +2,7 @@ package kr.co.dreamteams.dreamteams_android2;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -17,17 +18,26 @@ public class StepCheckService extends Service implements SensorEventListener {
     Common common = new Common(this);
 
     public static int count = StepValue.Step;
-    private long lastTime;
-    private float speed;
-    private float lastX;
-    private float lastY;
-    private float lastZ;
-
-    private float x, y, z;
-    private static final int SHAKE_THRESHOLD = 800;
 
     private SensorManager sensorManager;
     private Sensor accelerometerSensor;
+
+    private static final int ACCEL_RING_SIZE = 50;
+    private static final int VEL_RING_SIZE = 10;
+
+    // change this threshold according to your sensitivity preferences
+    private static final float STEP_THRESHOLD = 13;
+
+    private static final int STEP_DELAY_NS = 250000000;
+
+    private int accelRingCounter = 0;
+    private float[] accelRingX = new float[ACCEL_RING_SIZE];
+    private float[] accelRingY = new float[ACCEL_RING_SIZE];
+    private float[] accelRingZ = new float[ACCEL_RING_SIZE];
+    private int velRingCounter = 0;
+    private float[] velRing = new float[VEL_RING_SIZE];
+    private long lastStepTimeNs = 0;
+    private float oldVelocityEstimate = 0;
 
     @Override
     public void onCreate() {
@@ -52,6 +62,8 @@ public class StepCheckService extends Service implements SensorEventListener {
             sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_GAME);
         } // end of if
 
+
+
         return START_STICKY;
     } // end of onStartCommand
 
@@ -65,72 +77,98 @@ public class StepCheckService extends Service implements SensorEventListener {
         } // end of if
     } // end of onDestroy
 
+
+    public void calculate(long timeNs, float x, float y, float z){
+        float[] currentAccel = new float[3];
+        currentAccel[0] = x;
+        currentAccel[1] = y;
+        currentAccel[2] = z;
+
+        // First step is to update our guess of where the global z vector is.
+        accelRingCounter++;
+        accelRingX[accelRingCounter % ACCEL_RING_SIZE] = currentAccel[0];
+        accelRingY[accelRingCounter % ACCEL_RING_SIZE] = currentAccel[1];
+        accelRingZ[accelRingCounter % ACCEL_RING_SIZE] = currentAccel[2];
+
+        float[] worldZ = new float[3];
+        worldZ[0] = SensorFilter.sum(accelRingX) / Math.min(accelRingCounter, ACCEL_RING_SIZE);
+        worldZ[1] = SensorFilter.sum(accelRingY) / Math.min(accelRingCounter, ACCEL_RING_SIZE);
+        worldZ[2] = SensorFilter.sum(accelRingZ) / Math.min(accelRingCounter, ACCEL_RING_SIZE);
+
+        float normalization_factor = SensorFilter.norm(worldZ);
+
+        worldZ[0] = worldZ[0] / normalization_factor;
+        worldZ[1] = worldZ[1] / normalization_factor;
+        worldZ[2] = worldZ[2] / normalization_factor;
+
+        float currentZ = SensorFilter.dot(worldZ, currentAccel) - normalization_factor;
+        velRingCounter++;
+        velRing[velRingCounter % VEL_RING_SIZE] = currentZ;
+
+        float velocityEstimate = SensorFilter.sum(velRing);
+
+        if (velocityEstimate > 5) {
+            //common.log("vel : " + velocityEstimate);
+            //common.log("st  : " + Float.toString(STEP_THRESHOLD));
+            //common.log("time: " + Long.toString(timeNs-lastStepTimeNs));
+            //common.log("last: " + Long.toString(lastStepTimeNs));
+            //common.log("DELA: " + Long.toString(STEP_DELAY_NS));
+        }
+
+        if (velocityEstimate > STEP_THRESHOLD
+                && oldVelocityEstimate <= STEP_THRESHOLD
+                && (timeNs - lastStepTimeNs > STEP_DELAY_NS)) {
+            reportStep();
+            lastStepTimeNs = timeNs;
+
+        }
+
+        oldVelocityEstimate = velocityEstimate;
+    }
+
     @Override
     public void onSensorChanged(SensorEvent event) {
-        Log.i("onSensorChanged", "IN");
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            long currentTime = System.currentTimeMillis();
-            long gabOfTime = (currentTime - lastTime);
-
-            if (gabOfTime > 100) { //  gap of time of step count
-                Log.i("onSensorChanged_IF", "FIRST_IF_IN");
-                lastTime = currentTime;
-
-                x = event.values[0];
-                y = event.values[1];
-                z = event.values[2];
-
-                speed = Math.abs(x + y + z - lastX - lastY - lastZ) / gabOfTime * 12000;
-
-                if (speed > SHAKE_THRESHOLD) {
-                    Log.i("onSensorChanged_IF", "SECOND_IF_IN");
-                    Intent myFilteredResponse = new Intent("make.a.yong.manbo");
-
-                    StepValue.Step = count++;
-
-                    String msg = StepValue.Step + "";
-                    myFilteredResponse.putExtra("stepService", "오늘 걸음수 : " + msg);
-
-                    sendBroadcast(myFilteredResponse);
-
-                    if(count%10==0) {
-                        long now = System.currentTimeMillis();
-                        Date date = new Date(now);
-                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-                        //SimpleDateFormat sdf = new SimpleDateFormat("yyMMddHHmm");
-                        String today = sdf.format(date);
-
-                        common.putSP("step_count", String.valueOf(count));
-                        String step_record_date =  common.getSP("step_record_date");
-
-                        if(!step_record_date.equals(today)){
-                            //common.putSP("2018-10-20", "20");
-                            //common.putSP("2018-10-22", "22");
-                            //common.putSP("2018-10-24", "24");
-                            //common.putSP("2018-10-25", "25");
-                            //common.putSP("2018-10-26", "26");
-                            //common.putSP("2018-10-27", "27");
-                            //common.putSP("2018-10-28", "28");
-
-                            String lastday = common.getSP("step_record_date");
-                            common.putSP(lastday,String.valueOf(count));
-                            common.putSP("step_record_date", today);
-                            common.putSP("step_count", "0");
-                            StepValue.Step = 0;
-                            count = 0;
-                        }
-
-                    }
-
-                } // end of if
-
-                lastX = event.values[0];
-                lastY = event.values[1];
-                lastZ = event.values[2];
-            } // end of if
-        } // end of if
+            calculate(event.timestamp, event.values[0], event.values[1], event.values[2]);
+        }
 
     } // end of onSensorChanged
+
+    private void reportStep(){
+
+        //Intent myFilteredResponse = new Intent("make.a.yong.manbo");
+
+        StepValue.Step = count++;
+
+        //String msg = StepValue.Step + "";
+        //myFilteredResponse.putExtra("stepService", "오늘 걸음수 : " + msg);
+
+        //sendBroadcast(myFilteredResponse);
+        common.log("오늘걸음수 : " + count);
+
+        if(count%10==0) {
+            long now = System.currentTimeMillis();
+            Date date = new Date(now);
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            //SimpleDateFormat sdf = new SimpleDateFormat("yyMMddHHmm");
+            String today = sdf.format(date);
+
+            common.putSP("step_count", String.valueOf(count));
+            String step_record_date =  common.getSP("step_record_date");
+
+            if(!step_record_date.equals(today)){
+
+                String lastday = common.getSP("step_record_date");
+                common.putSP(lastday,String.valueOf(count));
+                common.putSP("step_record_date", today);
+                common.putSP("step_count", "0");
+                StepValue.Step = 0;
+                count = 0;
+            }
+
+        }
+
+    }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
